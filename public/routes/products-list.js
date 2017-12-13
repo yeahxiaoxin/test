e0633bb8b0402115b902e6b89337491bb2d6a854
yeahxiaoxin <yeahxiaoxin@163.com>
2017-12-13 17:10:25 +0800

    123
    123

--- public/routes/products-list.js
+++ public/routes/products-list.js
@@ -46,13 +46,13 @@ function setupData() {
 
 function logout() {
   AV.User.logOut();
-  window.location.href = "../../views/login.ejs";
+  window.location.href = "http://easybill.leanapp.cn/login.ejs";
 };
 
 $(function() {
   if (isCurrentUser()) {
     setupData();
   } else {
-    window.location.href = "../../views/login.ejs";
+    window.location.href = "http://easybill.leanapp.cn/login.ejs";
   }
 });
