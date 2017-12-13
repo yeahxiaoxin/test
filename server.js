1520de3d853a42aa83ac36d8d11c647f884ce4f1
yeahxiaoxin <yeahxiaoxin@163.com>
2017-12-13 15:33:23 +0800

    yeahxiaoxin
    123

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ AV.init({
     masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'D1zYOe90Gz040naN9hgKPyYu'
 });
 
-var app = express();
-app.use(AV.express());
-app.listen(process.env.LEANCLOUD_APP_PORT);
+// var app = express();
+// app.use(AV.express());
+// app.listen(process.env.LEANCLOUD_APP_PORT);
 
 
 //var AV = require('leanengine');
@@ -25,7 +25,7 @@ app.listen(process.env.LEANCLOUD_APP_PORT);
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
-//var app = require('./app');
+var app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
