7e904ac3710cf70bc5e20fc181bc7e331193bcf3
yeahxiaoxin <yeahxiaoxin@163.com>
2017-12-13 15:28:31 +0800

    yeahxiaoxin
    123

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 'use strict';
 
+var express = require('express');
 var AV = require('leanengine');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+    appId: process.env.LEANCLOUD_APP_ID || 'vWfTdVkt2d0C3sJ6Wy1h123V-gzGzoHsz',
+    appKey: process.env.LEANCLOUD_APP_KEY || 'qyJfdXdN8udwFJ88LlOwc9gc',
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'D1zYOe90Gz040naN9hgKPyYu'
 });
+
+var app = express();
+app.use(AV.express());
+app.listen(process.env.LEANCLOUD_APP_PORT);
+
+
+//var AV = require('leanengine');
+
+// AV.init({
+//   appId: process.env.LEANCLOUD_APP_ID,
+//   appKey: process.env.LEANCLOUD_APP_KEY,
+//   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+// });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
-var app = require('./app');
+//var app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
